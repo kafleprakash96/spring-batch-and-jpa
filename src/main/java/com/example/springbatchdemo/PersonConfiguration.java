5f1f13584ee1c5e6c070cb15f0d79c3c6ee19681
Prakash Kafle <kafleprakash96@gmail.com>
2024-08-17 22:57:22 -0500

    Refactored to make use of jpa respository to write file to db

--- src/main/java/com/example/springbatchdemo/PersonConfiguration.java
+++ src/main/java/com/example/springbatchdemo/PersonConfiguration.java
@@ -5,38 +5,21 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.database.JdbcBatchItemWriter;
-import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jdbc.metadata.HikariDataSourcePoolMetadata;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-
-import org.springframework.core.io.Resource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.PlatformTransactionManager;
-
-
-import javax.sql.DataSource;
 
 @Configuration
 @EnableJpaRepositories
 public class PersonConfiguration {
-    @Bean
-    public DataSource dataSource(){
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/spring-batch");
-        dataSource.setUsername( "root" );
-        dataSource.setPassword( "password" );
-        return dataSource;
-    }
+
+    @Autowired
+    private PersonItemWriter writer;
 
     @Bean
     public FlatFileItemReader<Person> reader(){
@@ -54,16 +37,6 @@ public class PersonConfiguration {
         return new PersonItemProcessor();
     }
 
-
-    @Bean
-    public JdbcBatchItemWriter<Person> writer(DataSource dataSource){
-        return new JdbcBatchItemWriterBuilder<Person>()
-                .sql("INSERT INTO person (first_name, last_name) VALUES (:firstName, :lastName)")
-                .dataSource(dataSource)
-                .beanMapped()
-                .build();
-    }
-
     @Bean
     public Job importPersonJob(JobRepository jobRepository,Step step1){
         return new JobBuilder("importPersonJob",jobRepository)
@@ -76,7 +49,7 @@ public class PersonConfiguration {
                       PlatformTransactionManager transactionManager,
                       FlatFileItemReader<Person> reader,
                       PersonItemProcessor processor,
-                      JdbcBatchItemWriter writer){
+                      PersonItemWriter writer){
         return new StepBuilder("step1",jobRepository)
                 .<Person,Person> chunk(5,transactionManager)
                 .reader(reader)
